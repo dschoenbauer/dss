5bc446d6174c2beea03a1f6fe1438fc69c5acb3f
David Schoenbauer <dschoenbauer@gmail.com>
2020-09-01 12:24:44 -0500

    task: #x looking for a better sizing option

--- js/Components/Canvas/FullSize.js
+++ js/Components/Canvas/FullSize.js
@@ -1,12 +1,12 @@
 export class FullSize {
 	visitCanvas(entity) {
 		let resize = (node) => {
-			let { width, height } = node.parentNode.getBoundingClientRect();
+			let { clientWidth : width, clientHeight: height } = node.parentNode;
 			Object.assign(node, { width, height })
 		}
 
 		resize(entity.canvas.dom);
-	
+
 		window.addEventListener("resize", () => {
 			resize(entity.canvas.dom);
 			entity.trigger("refresh");
